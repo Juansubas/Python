473ed917f9bd2d5531ea7606dc20bd73269e50e5
juansubas <juansubas@gmail.com>
2025-10-22 19:32:38 -0500

    Update People class to include age parameter and fix get_age method

--- poo/object.py
+++ poo/object.py
@@ -1,11 +1,15 @@
 class People:
     classname = 'Juansubas'
     
-    def __init__(self, name: str):
+    def __init__(self, name: str, age: int):
         self.name = name
+        self.__age = age
         
     def hi(self ):
         print("Hola " + self.name)
+        
+    def get_age(self):
+        return self.__Age
         
     @staticmethod
     def helloworld():
@@ -16,10 +20,12 @@ class People:
         print("Hola mundo2")
     
   
-juan = People("Juan")
+juan = People("Juan", 20)
 
 juan.hi()
 
 juan.helloworld()
 
 People.helloworld2()
+
+print(juan.get_age())
